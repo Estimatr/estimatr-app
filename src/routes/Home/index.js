bf5e8696fc6eff1b07d92ebcb5e198cb53b24bd0
Tanner <tanner.crockett@gmail.com>
2017-04-19 12:02:04 -0600

    Home page

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 // import { css } from 'glamor';
 
 export default () => (
-  <div>
 
+  <div>
+    <h1>The Estimatr</h1>
+
+    <h3>An easy way for a business to create an estimate for their customers</h3>
+
+    <h3>Frontend built with React</h3>
+
+    <h3>Firebase used for the database</h3>
   </div>
 );
+  
